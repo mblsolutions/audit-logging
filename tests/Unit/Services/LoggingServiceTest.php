a2d9b5183d1fb9c1156b66439422bfe7e5b602a2
Christian Innerbichler <christian.innerbichler@gmail.com>
2022-09-06 11:54:07 +0100

    fix failing test

--- tests/Unit/Services/LoggingServiceTest.php
+++ tests/Unit/Services/LoggingServiceTest.php
@@ -5,6 +5,8 @@ namespace MBLSolutions\AuditLogging\Tests\Unit\Services;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use MBLSolutions\AuditLogging\Services\Log\AuditLoggingService;
+use MBLSolutions\AuditLogging\Support\DTO\RequestResponseDTO;
+use MBLSolutions\AuditLogging\Support\Enums\LogType;
 use MBLSolutions\AuditLogging\Tests\LaravelTestCase;
 
 class LoggingServiceTest extends LaravelTestCase
@@ -13,7 +15,7 @@ class LoggingServiceTest extends LaravelTestCase
     /** @test **/
     public function can_create_audit_logging_service(): void
     {
-        $service = new AuditLoggingService(new Request([]), new Response([]));
+        $service = new AuditLoggingService(new RequestResponseDTO(LogType::WEB, new Request([]), new Response([])));
 
         $this->assertInstanceOf(AuditLoggingService::class, $service);
     }
